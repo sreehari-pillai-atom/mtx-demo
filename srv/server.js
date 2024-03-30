4a77dc8a152f33c4588de3dab211165301dd3c8b
Sreehari Pillai <sreehari.pillai@atomdn.com>
2024-03-30 18:43:01 +0000

    added destinations ervice , getDependencies

---
+++ srv/server.js
@@ -0,0 +1,26 @@
+const cds = require("@sap/cds");
+const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+const xsenv = require("@sap/xsenv");
+
+cds.on("bootstrap",  (app) => {
+    //provide a v2 URL
+    app.use(proxy());
+    //your own getDependencies URL , to override standard. 
+    app.get("/getDependencies", (req, res) => {
+         xsenv.loadEnv();
+         const services = xsenv.getServices({
+           dest: { tag: "destination" } //exponse estination services' xsapp 
+         });
+         if(services && services.dest){
+            cds.env.mtx.dependencies = [services.dest.xsappname];
+         }
+         else{
+            cds.env.mtx.dependencies = [];
+         }
+       
+         let deps = cds.env.mtx.dependencies.map((m) => {
+           return { "xsappname": m }
+         });
+         res.status(200).json(deps);
+       });
+  });
